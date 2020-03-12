efbd8464577a8fb3d33b421d4ac18ddeb5395296
Juliano Vaz <julianovaz@br-ufpe-05.local>
2020-03-12 10:26:49 -0300

    ˜Reorganizando pastas(grupos) e aprimorando arquivos de estoque e receitas (json)

---
+++ Foodalc/Model/Ingrediente.swift
@@ -0,0 +1,23 @@
+//
+//  Ingrediente.swift
+//  Foodalc
+//
+//  Created by Juliano Vaz on 10/03/20.
+//  Copyright © 2020 Juliano Vaz. All rights reserved.
+//
+
+import Foundation
+
+class Ingrediente {
+    var nome: String = ""
+    var qtd: Int = 0
+    var unidade: Float = 0.0
+    var unidadeDeMedida: String = ""
+    
+    init(nome: String, qtd: Int, unidade: Float, unidadeDeMedida: String) {
+        self.nome = nome
+        self.qtd = qtd
+        self.unidade = unidade
+        self.unidadeDeMedida = unidadeDeMedida
+    }
+}
---
+++ Foodalc/Model/Receita.swift
@@ -0,0 +1,23 @@
+//
+//  Receita.swift
+//  Foodalc
+//
+//  Created by Juliano Vaz on 10/03/20.
+//  Copyright © 2020 Juliano Vaz. All rights reserved.
+//
+
+import Foundation
+
+///Comentarios sobre documentacao de receita
+class Receita {
+    var ingredientes: [Ingrediente]
+    var nome: String = ""
+    var modoDePreparo: String = ""
+    
+    init(ingredientes: Array<Ingrediente>, nome: String, modoDePreparo: String) {
+        self.ingredientes = ingredientes
+        self.nome = nome
+        self.modoDePreparo = modoDePreparo
+        
+    }
+}
